5497e31205f48a801597bbba9f201e24cc9a1930
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-16 01:59:55 +0200

    :tada: Add static method to add/get all players laoded

--- src/main/java/fr/mrlizzard/wardevil/builder/objects/WardPlayer.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/objects/WardPlayer.java
@@ -1,18 +1,33 @@
 package fr.mrlizzard.wardevil.builder.objects;
 
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.InventoryView;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class WardPlayer {
 
-    private UUID                uuid;
-    private String              name;
+    private static Map<UUID, WardPlayer>    players = new HashMap<UUID, WardPlayer>();
+
+    private UUID                            uuid;
+    private String                          name;
 
     public WardPlayer(UUID uuid, String name) {
         this.uuid = uuid;
         this.name = name;
+
+        players.put(this.uuid, this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public static Map<UUID, WardPlayer> getPlayers() {
+        return players;
     }
 
 }
