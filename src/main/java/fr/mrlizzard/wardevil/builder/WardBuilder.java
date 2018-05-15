2fb234e8c60b6fd68592fea9bf00c534ae94dfdf
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-15 20:44:24 +0200

    :tea: Update pom.xml

--- src/main/java/fr/mrlizzard/wardevil/builder/WardBuilder.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/WardBuilder.java
@@ -1,4 +1,22 @@
 package fr.mrlizzard.wardevil.builder;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class WardBuilder extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+    
 }
