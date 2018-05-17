0586a838f4515a3de3b3b304163b7e93b598febc
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-17 17:58:29 +0200

    :tea: Implement whitelist in playerlogin listener

--- src/main/java/fr/mrlizzard/wardevil/builder/listeners/player/PlayerLoginListener.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/listeners/player/PlayerLoginListener.java
@@ -1,5 +1,7 @@
 package fr.mrlizzard.wardevil.builder.listeners.player;
 
+import fr.mrlizzard.wardevil.builder.WardBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,9 +9,22 @@ import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerLoginListener implements Listener {
 
+    private WardBuilder                 instance;
+
+    public PlayerLoginListener(WardBuilder instance) {
+        this.instance = instance;
+    }
+
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
+
+        if (!instance.getManager().getWhitelisted().contains(player.getUniqueId())) {
+            player.kickPlayer(ChatColor.RED + "Vous n'avez pas la permission de rentrer" +
+                    " sur ce serveur.");
+        }
+
+        // TODO: Check player configuration
     }
 
 }
