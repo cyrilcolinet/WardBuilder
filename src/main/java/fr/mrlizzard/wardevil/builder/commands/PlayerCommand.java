07d2c9e56ed029859b52afe64e76f6595c6ec9cb
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-19 20:29:51 +0200

    :tada: Add info command in player subcommand

--- src/main/java/fr/mrlizzard/wardevil/builder/commands/PlayerCommand.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/commands/PlayerCommand.java
@@ -3,6 +3,8 @@ package fr.mrlizzard.wardevil.builder.commands;
 import fr.mrlizzard.wardevil.builder.WardBuilder;
 import fr.mrlizzard.wardevil.builder.uitls.Rank;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import redis.clients.jedis.Jedis;
 
 import java.util.Map;
 import java.util.UUID;
@@ -17,15 +19,17 @@ public class PlayerCommand extends ACommand {
     public void loadSubCommands() {
         subCommands.put("promote", () -> promotePlayer());
         subCommands.put("whitelist", () -> whitelistPlayer());
+        subCommands.put("info", () -> infoPlayer());
     }
 
     @Override
     public void displayHelp() {
         sender.sendMessage("§c--[ §6WardBuilder | Players Help §c]--");
-        sender.sendMessage("§e /build players help §f- §6Afficher la page d'aide (cette page)");
-        sender.sendMessage("§e /build players promote <player> <rank> §f- §6Promouvoir un joueur");
-        sender.sendMessage("§e /build players whitelist add <player> §f- §6Whitelister un joueur");
-        sender.sendMessage("§e /build players whitelist del <player> §f- §6Dé-Whitelister un joueur");
+        sender.sendMessage("§e /build players help §f- §6Afficher la page d'aide");
+        sender.sendMessage("§e /build players promote <player> <rank> §f- §6Promouvoir");
+        sender.sendMessage("§e /build players whitelist add <player> §f- §6Whitelister");
+        sender.sendMessage("§e /build players whitelist del <player> §f- §6Dé-Whitelister");
+        sender.sendMessage("§e /build players info <player> §f- §6Voir les infos");
     }
 
     private void promotePlayer() {
@@ -56,6 +60,34 @@ public class PlayerCommand extends ACommand {
 
     private void whitelistPlayer() {
         sender.sendMessage("cc");
+    }
+
+    private void infoPlayer() {
+        UUID uuid;
+        Jedis jedis;
+        Map<String, String> values;
+
+        if (args.length < 3) {
+            sender.sendMessage("§cUsage: /build players info <player>");
+            return;
+        }
+
+        uuid = instance.getUuidTranslator().getUUID(args[2], true);
+        if (uuid == null) {
+            sender.sendMessage("§cAucun joueur nommé " + args[2] + " trouvé.");
+            return;
+        }
+
+        jedis = instance.getConnector().getRessource();
+
+        if (!jedis.exists("players:" + uuid)) {
+            sender.sendMessage("§cAucune données présentes concernant " + args[2]);
+            return;
+        }
+
+        values = jedis.hgetAll("players:" + uuid);
+        sender.sendMessage("§aVoici les données concernant " + args[2]);
+        values.forEach((key, value) -> sender.sendMessage("  §e" + key + " §c-> §b" + value));
     }
 
     @Override
