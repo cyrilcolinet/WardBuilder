b151235ccb316dc7cf6566b3f44c10abd88a7caf
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-21 12:12:39 +0200

    :tada: Add help for worlds command

--- src/main/java/fr/mrlizzard/wardevil/builder/commands/WorldCommand.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/commands/WorldCommand.java
@@ -15,7 +15,16 @@ public class WorldCommand extends ACommand {
 
     @Override
     public void displayHelp() {
-
+        sender.sendMessage("§c--[ §6WardBuilder | Worlds Help §c]--");
+        sender.sendMessage("§e /build worlds help §f- §6Afficher la page d'aide");
+        sender.sendMessage("§e /build worlds list §f- §6Afficher la liste");
+        sender.sendMessage("§e /build worlds add <world> §f- §6Ajouter");
+        sender.sendMessage("§e /build worlds del <world> §f- §6Supprimer");
+        sender.sendMessage("§e /build worlds disable <world> §f- §6Désactiver");
+        sender.sendMessage("§e /build worlds enable <world> §f- §6Activer");
+        sender.sendMessage("§e /build worlds players list <world> §f- §6Liste des joueurs");
+        sender.sendMessage("§e /build worlds players add <world> <name> §f- §6Ajouter joueur");
+        sender.sendMessage("§e /build worlds players del <world> <name> §f- §6Supprimer joueur");
     }
 
     @Override
