94ce76f47cfeb61a42f7f263aa4916c1ce979500
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-22 23:03:05 +0200

    :tada: Add map deconnection

--- src/main/java/fr/mrlizzard/wardevil/builder/listeners/player/PlayerDisconnectListener.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/listeners/player/PlayerDisconnectListener.java
@@ -2,6 +2,7 @@ package fr.mrlizzard.wardevil.builder.listeners.player;
 
 import fr.mrlizzard.wardevil.builder.WardBuilder;
 import fr.mrlizzard.wardevil.builder.objects.BuildPlayer;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,10 +20,19 @@ public class PlayerDisconnectListener implements Listener {
     public void onPlayerDisconnect(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         BuildPlayer buildPlayer = instance.getManager().getPlayer(player.getUniqueId());
+        World world = player.getWorld();
 
         if (buildPlayer == null) {
             instance.getLog().error("Profile for " + player.getUniqueId().toString() + " is null!");
             return;
+        }
+
+        if (!world.getName().equalsIgnoreCase("world")) {
+            instance.getServer().getOnlinePlayers().stream().filter(p -> p.getName().equalsIgnoreCase(world.getName()))
+                    .forEach(p -> {
+                p.sendMessage("§e<> " + buildPlayer.getRank().getPrefix() + player.getName() +
+                        " §cs'est déconnecté de ce monde. §e<>");
+            });
         }
 
         event.setQuitMessage(buildPlayer.getRank().getPrefix() + player.getName() + " §6nous a quitté.");
