320789dcb1f6ce77592d071df3edf615ce0df8ae
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-22 21:35:58 +0200

    :tada: Add block listeners

--- src/main/java/fr/mrlizzard/wardevil/builder/listeners/ListenersManager.java
+++ src/main/java/fr/mrlizzard/wardevil/builder/listeners/ListenersManager.java
@@ -2,6 +2,7 @@ package fr.mrlizzard.wardevil.builder.listeners;
 
 import fr.mrlizzard.wardevil.builder.WardBuilder;
 import fr.mrlizzard.wardevil.builder.listeners.player.*;
+import fr.mrlizzard.wardevil.builder.listeners.server.BlockListener;
 import fr.mrlizzard.wardevil.builder.listeners.server.ServerListener;
 import org.bukkit.plugin.PluginManager;
 
@@ -23,6 +24,7 @@ public class ListenersManager {
         pluginManager.registerEvents(new ServerListener(instance), instance);
         pluginManager.registerEvents(new AsyncPlayerChatListener(), instance);
         pluginManager.registerEvents(new PlayerMiscListener(instance), instance);
+        pluginManager.registerEvents(new BlockListener(instance), instance);
     }
 
 }
---
+++ src/main/java/fr/mrlizzard/wardevil/builder/listeners/server/BlockListener.java
@@ -0,0 +1,103 @@
+package fr.mrlizzard.wardevil.builder.listeners.server;
+
+import fr.mrlizzard.wardevil.builder.WardBuilder;
+import fr.mrlizzard.wardevil.builder.objects.World;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class BlockListener implements Listener {
+
+    private WardBuilder                 instance;
+
+    public BlockListener(WardBuilder instance) {
+        this.instance = instance;
+    }
+
+    @EventHandler
+    public void onBlockBlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Boolean onSpawn = event.getBlock().getWorld().getName().equals("world");
+
+        if (onSpawn && !player.isOp()) {
+            event.setCancelled(true);
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+
+        if (!onSpawn) {
+            World world = instance.getWorldManager().getWorlds().get(event.getBlock().getWorld().getName());
+            if (world == null)
+                return;
+
+            if (world.getBuilders().containsKey(player.getUniqueId())) {
+                event.setCancelled(true);
+                player.sendMessage("§cVous n'avez pas la permission ici.");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockDamage(BlockDamageEvent event) {
+        Player player = event.getPlayer();
+        Boolean onSpawn = event.getBlock().getWorld().getName().equals("world");
+
+        if (onSpawn && !player.isOp()) {
+            event.setCancelled(true);
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+
+        if (!onSpawn) {
+            World world = instance.getWorldManager().getWorlds().get(event.getBlock().getWorld().getName());
+            if (world == null)
+                return;
+
+            if (world.getBuilders().containsKey(player.getUniqueId())) {
+                event.setCancelled(true);
+                player.sendMessage("§cVous n'avez pas la permission ici.");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Boolean onSpawn = event.getBlock().getWorld().getName().equals("world");
+
+        if (onSpawn && !player.isOp()) {
+            event.setCancelled(true);
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+
+        if (!onSpawn) {
+            World world = instance.getWorldManager().getWorlds().get(event.getBlock().getWorld().getName());
+            if (world == null)
+                return;
+
+            if (world.getBuilders().containsKey(player.getUniqueId())) {
+                event.setCancelled(true);
+                player.sendMessage("§cVous n'avez pas la permission ici.");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event)
+    {
+        event.blockList().clear();
+    }
+
+    @EventHandler
+    public void onBlockSpread(BlockSpreadEvent event) {
+        if (event.getBlock().getType() != Material.getMaterial(2) || event.getBlock().getType() != Material.getMaterial(3)) {
+            event.setCancelled(true);
+        }
+    }
+
+}
